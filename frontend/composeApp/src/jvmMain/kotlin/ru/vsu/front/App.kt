8c9e162abff7431334863e1e262b1651d5600168
stepann <stepan.snegovskoi@mail.ru>
2026-02-16 20:57:41 +0300

    Initial commit

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/App.kt
@@ -0,0 +1,13 @@
+package ru.vsu.front
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+    }
+}
