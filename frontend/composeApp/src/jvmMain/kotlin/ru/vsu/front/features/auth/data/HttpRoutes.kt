7dd3c128ba92d8bf6d218b03a66c88feb00b586e
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 19:01:46 +0300

    create http routes

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/data/HttpRoutes.kt
@@ -0,0 +1,17 @@
+@file:Suppress("SpellCheckingInspection")
+
+package ru.vsu.front.features.auth.data
+
+/**
+ * Http Маршруты
+ */
+object HttpRoutes {
+    /**
+     * Эндпоинт авторизации
+     */
+    const val LOGIN = "/login"
+    /**
+     * Эндпоинт регистрации
+     */
+    const val SIGN = "/sign"
+}
