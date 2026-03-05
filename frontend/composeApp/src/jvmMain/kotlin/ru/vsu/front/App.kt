31beb0c0890dd0358ed52df33fe87183fefebcd9
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 22:10:44 +0300

    update App root composable function

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/App.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/App.kt
@@ -1,13 +1,40 @@
 package ru.vsu.front
 
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.WindowScope
+import ru.vsu.front.designsystem.component.WindowTopBar
+import ru.vsu.front.designsystem.theme.CodeTogetherTheme
+import ru.vsu.front.designsystem.theme.CodeTogetherThemeVariant
 
 @Composable
-@Preview
-fun App() {
-    MaterialTheme {
+fun WindowScope.App(
+    onMinimizeClick: () -> Unit,
+    onMaximizeClick: () -> Unit,
+    onCloseClick: () -> Unit,
+    content: @Composable () -> Unit = {}
+) {
+    CodeTogetherTheme(
+        themeVariant = CodeTogetherThemeVariant.Omni
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            WindowTopBar(
+                onMinimizeClick = onMinimizeClick,
+                onMaximizeClick = onMaximizeClick,
+                onCloseClick = onCloseClick,
+            )
 
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(CodeTogetherTheme.colors.primaryBackground)
+            ) {
+                content()
+            }
+        }
     }
 }
