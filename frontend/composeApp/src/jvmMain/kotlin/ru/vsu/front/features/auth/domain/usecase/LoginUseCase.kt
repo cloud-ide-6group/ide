ad381146ba2b118754e774425890fdd67ec04c7d
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 19:05:07 +0300

    create LoginUseCase

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/domain/usecase/LoginUseCase.kt
@@ -0,0 +1,22 @@
+package ru.vsu.front.features.auth.domain.usecase
+
+import ru.vsu.front.features.auth.domain.entity.AuthResult
+import ru.vsu.front.features.auth.domain.entity.UserSession
+import ru.vsu.front.features.auth.domain.repository.AuthRepository
+
+/**
+ * Юзкейс авторизации
+ *
+ * @param repository Репозиторий с реализацией логина, регистрации
+ */
+class LoginUseCase(
+    private val repository: AuthRepository,
+) {
+    /**
+     * @param email Почта пользователя
+     * @param password Пароль пользователя
+     */
+    suspend operator fun invoke(email: String, password: String): AuthResult<UserSession> {
+        return repository.login(email = email, password = password)
+    }
+}
