9c18c69b85ad5fe1b1c3b9a73c9484b70831c091
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 19:02:21 +0300

    create httpClient

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/data/httpClient.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/data/httpClient.kt
@@ -3,8 +3,10 @@ package ru.vsu.front.features.auth.data
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import ru.vsu.front.config.BuildKonfig
 
 /**
  * HttpClient
@@ -16,6 +18,9 @@ fun httpClient(): HttpClient {
                 prettyPrint = true
                 ignoreUnknownKeys = true
             })
+        }
+        defaultRequest {
+            url(BuildKonfig.BASE_URL)
         }
     }
 }
