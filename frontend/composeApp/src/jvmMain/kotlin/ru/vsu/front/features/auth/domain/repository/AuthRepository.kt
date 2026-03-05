cd5563a5df70fbef85a6b755e119a0b437012138
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 18:21:35 +0300

    create AuthRepository

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/domain/repository/AuthRepository.kt
@@ -0,0 +1,27 @@
+@file:Suppress("SpellCheckingInspection")
+
+package ru.vsu.front.features.auth.domain.repository
+
+import ru.vsu.front.features.auth.domain.entity.AuthResult
+import ru.vsu.front.features.auth.domain.entity.UserSession
+
+/**
+ * Репозиторий, ответственный за авторизацию и регистрацию
+ */
+interface AuthRepository {
+    /**
+     * Функция авторизации
+     *
+     * @param email Почта пользователя
+     * @param password Пароль пользователя
+     */
+    suspend fun login(email: String, password: String): AuthResult<UserSession>
+    /**
+     * Функция регистрации
+     *
+     * @param name Имя пользователя
+     * @param email Почта пользователя
+     * @param password Пароль пользователя
+     */
+    suspend fun sign(name: String, email: String, password: String): AuthResult<UserSession>
+}
