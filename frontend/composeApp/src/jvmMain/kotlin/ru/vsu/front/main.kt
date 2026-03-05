8ca3b3ae8a7fc8b25900689abaa5ec09b3b30a25
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 19:06:14 +0300

    call initKoin in main

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/main.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/main.kt
@@ -2,12 +2,16 @@ package ru.vsu.front
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import ru.vsu.front.features.auth.di.initKoin
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "front",
-    ) {
-        App()
+fun main() {
+    initKoin()
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "front",
+        ) {
+            App()
+        }
     }
 }
