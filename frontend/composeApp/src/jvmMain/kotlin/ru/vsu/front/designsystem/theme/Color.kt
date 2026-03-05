0c80d90216729f1d835b2c826a8cf930b6ba9814
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 14:38:05 +0300

    create omni palette

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/designsystem/theme/Color.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/designsystem/theme/Color.kt
@@ -1,21 +1,17 @@
+@file:Suppress("SpellCheckingInspection")
+
 package ru.vsu.front.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
 /**
- * Colors
+ * Omni палитра [В текущей реализации цвета только для экрана логина]
  */
-val DeepDark100 = Color(0xFF13111B)
-val DeepDark90 = Color(0xFF191622)
-val DeepDark80 = Color(0xFF211E2D)
-val DeepDark70 = Color(0xFF413C53)
-val Slate60 = Color(0xFF44475A)
-val Slate50 = Color(0xFF5A4B81)
-val Slate40 = Color(0xFF6272A4)
-val White90 = Color(0xFFE1E1E6)
-val Pink50 = Color(0xFFFF79C6)
-val Purple50 = Color(0xFFBD93F9)
-val Cyan50 = Color(0xFF8BE9FD)
-val Green50 = Color(0xFF50FA7B)
-val Yellow50 = Color(0xFFF1FA8C)
-val Red50 = Color(0xFFFF5555)
+val omniPalette = CodeTogetherColors(
+    primary = Color(0xFFFF79C6),
+    primaryText = Color(0xFFE1E1E6),
+    primaryBackground = Color(0xFF13111B),
+    secondaryText = Color(0x80888888),
+    secondaryBackground = Color(0xFF191622),
+    error = Color(0xFFFF5555),
+)
