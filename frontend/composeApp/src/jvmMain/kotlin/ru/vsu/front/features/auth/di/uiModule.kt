f31e602b7f164eebb2628d7a85d168bf7eabee72
stepann <stepan.snegovskoi@mail.ru>
2026-03-06 00:27:26 +0300

    add comments to uiModule

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/di/uiModule.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/di/uiModule.kt
@@ -1,9 +1,16 @@
+@file:Suppress("SpellCheckingInspection")
+
 package ru.vsu.front.features.auth.di
 
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import ru.vsu.front.features.auth.ui.LoginViewModel
 
+/**
+ * Модуль слоя ui
+ *
+ * @see LoginViewModel Предоставление вьюмодели экрана логина, Singletone
+ */
 val uiModule = module {
     viewModelOf(::LoginViewModel)
 }
