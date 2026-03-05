c468444e7a8f905a856a7ff48c493bc4d9dbdc44
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 22:12:17 +0300

    base init LoginViewModel

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/ui/LoginViewModel.kt
@@ -0,0 +1,58 @@
+package ru.vsu.front.features.auth.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ru.vsu.front.features.auth.domain.usecase.LoginUseCase
+import ru.vsu.front.features.auth.domain.usecase.SignUseCase
+
+class LoginViewModel(
+    private val loginUseCase: LoginUseCase,
+    private val signUseCase: SignUseCase,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState>(UiState())
+    val uiState: StateFlow<UiState>
+        get() = _uiState.asStateFlow()
+
+    fun processCommand(command: LoginCommand) {
+        when(val command = command) {
+            is LoginCommand.ChangeEmail -> {
+                _uiState.update { previousState ->
+                    previousState.copy(email = command.email)
+                }
+            }
+
+            is LoginCommand.ChangePassword -> {
+                _uiState.update { previousState ->
+                    previousState.copy(password = command.password)
+                }
+            }
+
+            LoginCommand.ClickLogin -> {
+                viewModelScope.launch(Dispatchers.IO) {
+                    loginUseCase(
+                        email = _uiState.value.email,
+                        password = _uiState.value.password
+                    )
+                }
+            }
+        }
+    }
+}
+
+sealed interface LoginCommand {
+    data class ChangeEmail(val email: String) : LoginCommand
+    data class ChangePassword(val password: String) : LoginCommand
+    data object ClickLogin : LoginCommand
+}
+
+data class UiState(
+    val email: String = "",
+    val password: String = ""
+)
