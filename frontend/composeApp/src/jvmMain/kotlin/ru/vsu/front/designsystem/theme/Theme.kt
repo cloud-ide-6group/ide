293faa90d2482d7b0fbb271c2bce848a35239762
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 14:38:49 +0300

    create CodeTogetherTheme

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/designsystem/theme/Theme.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/designsystem/theme/Theme.kt
@@ -1,28 +1,19 @@
 package ru.vsu.front.designsystem.theme
 
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
-/**
- * Omni theme
- */
-val omniColorScheme = darkColorScheme(
-    primary = Pink50,
-    onPrimary = DeepDark90,
-    primaryContainer = Green50,
-    secondaryContainer = Yellow50,
-    secondary = Cyan50,
-    onSecondary = DeepDark90,
-    tertiary = Purple50,
-    background = DeepDark90,
-    onBackground = White90,
-    surface = DeepDark100,
-    onSurface = White90,
-    surfaceTint = DeepDark80,
-    surfaceVariant = DeepDark70,
-    onSurfaceVariant = Slate40,
-    error = Red50,
-    onError = DeepDark90,
-    outline = Slate60,
-    outlineVariant = Slate50
-)
+@Composable
+fun CodeTogetherTheme(
+    themeVariant: CodeTogetherThemeVariant,
+    content: @Composable () -> Unit
+) {
+    val colors = when(themeVariant) {
+        CodeTogetherThemeVariant.Omni -> omniPalette
+    }
+
+    CompositionLocalProvider(
+        LocalCodeTogetherColors provides colors,
+        content = content
+    )
+}
