1aba6bc8b669bf7685a2a0310076d3d6b0434880
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 12:35:24 +0300

    add omni theme colors

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/designsystem/theme/Color.kt
@@ -0,0 +1,21 @@
+package ru.vsu.front.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Colors
+ */
+val DeepDark100 = Color(0xFF13111B)
+val DeepDark90 = Color(0xFF191622)
+val DeepDark80 = Color(0xFF211E2D)
+val DeepDark70 = Color(0xFF413C53)
+val Slate60 = Color(0xFF44475A)
+val Slate50 = Color(0xFF5A4B81)
+val Slate40 = Color(0xFF6272A4)
+val White90 = Color(0xFFE1E1E6)
+val Pink50 = Color(0xFFFF79C6)
+val Purple50 = Color(0xFFBD93F9)
+val Cyan50 = Color(0xFF8BE9FD)
+val Green50 = Color(0xFF50FA7B)
+val Yellow50 = Color(0xFFF1FA8C)
+val Red50 = Color(0xFFFF5555)
