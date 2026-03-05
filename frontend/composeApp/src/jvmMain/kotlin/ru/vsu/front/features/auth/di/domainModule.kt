2e3ca325bdad0cd1d7f967063527369e7930fb5f
stepann <stepan.snegovskoi@mail.ru>
2026-03-06 00:27:11 +0300

    add comments to domainModule

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/di/domainModule.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/di/domainModule.kt
@@ -1,9 +1,17 @@
+@file:Suppress("SpellCheckingInspection")
+
 package ru.vsu.front.features.auth.di
 
 import org.koin.dsl.module
 import ru.vsu.front.features.auth.domain.usecase.LoginUseCase
 import ru.vsu.front.features.auth.domain.usecase.SignUseCase
 
+/**
+ * Модуль слоя domain
+ *
+ * @see LoginUseCase Предоставление юзкейса логина, Singletone
+ * @see SignUseCase Предоставление юзкейса регистрации, Singletone
+ */
 val domainModule = module {
     single {
         LoginUseCase(get())
