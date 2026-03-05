f74b126aaad018992ab437d9f488e8009dc1c5d5
stepann <stepan.snegovskoi@mail.ru>
2026-03-05 12:44:22 +0300

    create omni color scheme

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/designsystem/theme/Theme.kt
@@ -0,0 +1,28 @@
+package ru.vsu.front.designsystem.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.ui.graphics.Color.Companion.White
+
+/**
+ * Omni theme
+ */
+val omniColorScheme = darkColorScheme(
+    primary = Pink50,
+    onPrimary = DeepDark90,
+    primaryContainer = Green50,
+    secondaryContainer = Yellow50,
+    secondary = Cyan50,
+    onSecondary = DeepDark90,
+    tertiary = Purple50,
+    background = DeepDark90,
+    onBackground = White90,
+    surface = DeepDark100,
+    onSurface = White90,
+    surfaceTint = DeepDark80,
+    surfaceVariant = DeepDark70,
+    onSurfaceVariant = Slate40,
+    error = Red50,
+    onError = DeepDark90,
+    outline = Slate60,
+    outlineVariant = Slate50
+)
