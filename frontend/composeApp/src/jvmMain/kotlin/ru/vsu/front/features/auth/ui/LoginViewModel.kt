c0ef7735c5b863bc0c8686153c83c591f158e4cd
stepann <stepan.snegovskoi@mail.ru>
2026-03-07 21:40:16 +0300

    update LoginViewModel: add comments, add isPasswordVisible to UiState, add ChangePasswordVisibility command

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/ui/LoginViewModel.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/ui/LoginViewModel.kt
@@ -1,3 +1,5 @@
+@file:Suppress("SpellCheckingInspection")
+
 package ru.vsu.front.features.auth.ui
 
 import androidx.lifecycle.ViewModel
@@ -9,8 +11,12 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import ru.vsu.front.features.auth.domain.usecase.LoginUseCase
-import ru.vsu.front.features.auth.domain.usecase.SignUseCase
 
+/**
+ * Вьюмодель экрана логина
+ *
+ * @param loginUseCase Юзкейс авторизации
+ */
 class LoginViewModel(
     private val loginUseCase: LoginUseCase
 ) : ViewModel() {
@@ -41,17 +47,40 @@ class LoginViewModel(
                     )
                 }
             }
+
+            LoginCommand.ChangePasswordVisibility -> {
+                _uiState.update { previousState ->
+                    previousState.copy(isPasswordVisible = !previousState.isPasswordVisible)
+                }
+            }
         }
     }
 }
 
+/**
+ * Комманды для вьюмодели логина
+ *
+ * @see ChangeEmail Сменить почту
+ * @see ChangePassword Сменить пароль
+ * @see ChangePasswordVisibility Сменить видимость пароля
+ * @see ClickLogin Нажатие на кнопку Login
+ */
 sealed interface LoginCommand {
     data class ChangeEmail(val email: String) : LoginCommand
     data class ChangePassword(val password: String) : LoginCommand
+    data object ChangePasswordVisibility : LoginCommand
     data object ClickLogin : LoginCommand
 }
 
+/**
+ * Состояние экрана логина
+ *
+ * @see email Текущая почта
+ * @see password текущий пароль
+ * @see isPasswordVisible Видимость пароля
+ */
 data class UiState(
     val email: String = "",
-    val password: String = ""
+    val password: String = "",
+    val isPasswordVisible: Boolean = false
 )
