4acd1e30b4a8acfa892813c2125925079684d7cb
stepann <stepan.snegovskoi@mail.ru>
2026-03-07 22:32:02 +0300

    add SignViewModel provide in uiModule

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/di/uiModule.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/di/uiModule.kt
@@ -5,12 +5,15 @@ package ru.vsu.front.features.auth.di
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import ru.vsu.front.features.auth.ui.LoginViewModel
+import ru.vsu.front.features.auth.ui.SignViewModel
 
 /**
  * Модуль слоя ui
  *
  * @see LoginViewModel Предоставление вьюмодели экрана логина
+ * @see SignViewModel Предоставление вьюмодели экрана регистрации
  */
 val uiModule = module {
     viewModelOf(::LoginViewModel)
+    viewModelOf(::SignViewModel)
 }
