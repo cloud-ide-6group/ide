dfef1b3c5c3fe10cc17850a78469af336dee2b07
stepann <stepan.snegovskoi@mail.ru>
2026-03-07 20:44:31 +0300

    the test use of LoginScreen, setup window

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/main.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/main.kt
@@ -1,18 +1,42 @@
+@file:Suppress("SpellCheckingInspection")
+
 package ru.vsu.front
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowScope
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import ru.vsu.front.designsystem.common.NecessaryAppButtons
+import ru.vsu.front.designsystem.theme.CodeTogetherTheme
 import ru.vsu.front.features.auth.di.initKoin
+import ru.vsu.front.features.auth.ui.LoginScreen
+import java.awt.Dimension
 
+/**
+ * Точка входа в программу
+ */
 fun main() {
     initKoin()
     application {
+        val windowState = rememberWindowState()
         Window(
             onCloseRequest = ::exitApplication,
             title = "front",
+            state = windowState,
             undecorated = true,
             transparent = true
         ) {
+            val density = LocalDensity.current
+            setupWindow(density)
+
             App(
                 onMinimizeClick = {
 
@@ -22,8 +46,30 @@ fun main() {
                 },
                 onCloseClick = ::exitApplication
             ) {
-
+                LoginScreen(
+                    modifier = Modifier
+                        .background(CodeTogetherTheme.colors.secondaryBackground)
+                        .padding(32.dp)
+                )
             }
+        }
+    }
+}
+
+/**
+ * Первоначальная настройка окна
+ *
+ * @param density Коэффициент плотности пикселей
+ */
+@Composable
+private fun WindowScope.setupWindow(density: Density) {
+    LaunchedEffect(density) {
+        window.minimumSize = with(density) {
+            Dimension(
+                (NecessaryAppButtons.entries.size * NecessaryAppButtons.NECESSARY_BUTTON_SIZE_IN_DP).dp.toPx()
+                    .toInt(),
+                NecessaryAppButtons.NECESSARY_BUTTON_SIZE_IN_DP.dp.toPx().toInt(),
+            )
         }
     }
 }
