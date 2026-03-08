02bdbb6179a3833c2aede44b0e990f29b0a0dc19
stepann <stepan.snegovskoi@mail.ru>
2026-03-08 23:35:52 +0300

    create appModule

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/common/security/di/appModule.kt
@@ -0,0 +1,35 @@
+package ru.vsu.front.common.security.di
+
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import org.koin.dsl.module
+import ru.vsu.front.common.security.CryptoManager
+import ru.vsu.front.common.security.TokenStorage
+import ru.vsu.front.common.security.getAppPreferencesDirectory
+import java.io.File
+
+/**
+ * Модуль приложения
+ *
+ * @see CryptoManager Предоставление класса шифрования
+ * @see PreferenceDataStoreFactory Предоставление хранилища из библиотеки datastore.preferences
+ * @see TokenStorage Предоставление хранилища токенов
+ */
+val appModule = module {
+
+    single {
+        CryptoManager()
+    }
+
+    single {
+        PreferenceDataStoreFactory.create(
+            produceFile = {
+                val appDir = getAppPreferencesDirectory()
+                File(appDir, "auth.preferences_pb")
+            }
+        )
+    }
+
+    single {
+        TokenStorage(dataStore = get(), cryptoManager = get())
+    }
+}
