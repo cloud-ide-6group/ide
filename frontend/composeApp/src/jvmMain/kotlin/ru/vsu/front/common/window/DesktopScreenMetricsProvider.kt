16c29c907c140da5d7624d3a8768cf51f7fc0f23
stepann <stepan.snegovskoi@mail.ru>
2026-03-08 21:13:36 +0300

    create DesktopScreenMetricsProvider

---
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/common/window/DesktopScreenMetricsProvider.kt
@@ -0,0 +1,22 @@
+@file:Suppress("SpellCheckingInspection")
+
+package ru.vsu.front.common.window
+
+import java.awt.GraphicsEnvironment
+
+/**
+ * Провайдер для получения максимального размера окна на Desktop
+ */
+object DesktopScreenMetricsProvider : ScreenMetricsProvider {
+    override fun getMaximumWindowBounds(): WindowBounds {
+        val env = GraphicsEnvironment.getLocalGraphicsEnvironment()
+        val maximumWindowBounds = env.maximumWindowBounds
+
+        return WindowBounds(
+            width = maximumWindowBounds.width,
+            height = maximumWindowBounds.height,
+            x = maximumWindowBounds.x,
+            y = maximumWindowBounds.y
+        )
+    }
+}
