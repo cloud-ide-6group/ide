612c1938ae05d75d6be278de863398fa8b102918
stepann <stepan.snegovskoi@mail.ru>
2026-03-18 18:16:41 +0300

    create EmailMatcher tests

--- frontend/composeApp/src/jvmTest/kotlin/ru/vsu/front/ComposeAppDesktopTest.kt
+++ frontend/composeApp/src/jvmTest/kotlin/ru/vsu/front/ComposeAppDesktopTest.kt
@@ -1,12 +1,24 @@
 package ru.vsu.front
 
+import ru.vsu.front.features.auth.domain.validation.EmailMatcher
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ComposeAppDesktopTest {
 
     @Test
-    fun example() {
-        assertEquals(3, 1 + 2)
+    fun `if email is dfgwer it's false`() {
+        assertEquals(false, EmailMatcher.isValid("dfgwer"))
+    }
+
+    @Test
+    fun `if email is dgecrec@ it's false`() {
+        assertEquals(false, EmailMatcher.isValid("dgecrec@"))
+    }
+
+    @Test
+    fun `if email is valid it's true`() {
+        assertEquals(true, EmailMatcher.isValid("mail@mail.ru"))
+        assertEquals(true, EmailMatcher.isValid("mail.mail@mail.ru"))
     }
 }
