47e2095c51a31c3d04144aad9ba78662128e426b
stepann <stepan.snegovskoi@mail.ru>
2026-03-20 22:46:56 +0300

    update regex in EmailMatcher

--- frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/domain/validation/EmailMatcher.kt
+++ frontend/composeApp/src/jvmMain/kotlin/ru/vsu/front/features/auth/domain/validation/EmailMatcher.kt
@@ -4,7 +4,7 @@ package ru.vsu.front.features.auth.domain.validation
  * Валидатор почты
  */
 object EmailMatcher {
-    private val EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$".toRegex()
+    private val EMAIL_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,12}\$".toRegex()
 
     fun isValid(email: String): Boolean = email.matches(EMAIL_REGEX)
 }
