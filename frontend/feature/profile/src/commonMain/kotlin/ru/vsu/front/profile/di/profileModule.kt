5228bc438e4a537a9c74ab80fa5fe70d18252c6c
stepann <stepan.snegovskoi@mail.ru>
2026-04-02 00:22:57 +0300

    create profileModule

---
+++ frontend/feature/profile/src/commonMain/kotlin/ru/vsu/front/profile/di/profileModule.kt
@@ -0,0 +1,17 @@
+package ru.vsu.front.profile.di
+
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+import ru.vsu.front.profile.ProfileViewModel
+
+/**
+ * Модуль профиля.
+ * * Отвечает за предоставление ViewModel для экранов.
+ * * Что внутри:
+ * - [ProfileViewModel] - вьюмодель экрана профиля.
+ */
+val profileModule = module {
+    viewModel { (userId: Int) ->
+        ProfileViewModel(userId = userId, get())
+    }
+}
