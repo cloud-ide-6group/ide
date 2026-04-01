54870d504784b604e05026702b733bb48a0bc81a
stepann <stepan.snegovskoi@mail.ru>
2026-04-02 00:31:21 +0300

    update Navigation

--- frontend/app/src/jvmMain/kotlin/ru/vsu/front/navigation/Navigation.kt
+++ frontend/app/src/jvmMain/kotlin/ru/vsu/front/navigation/Navigation.kt
@@ -6,9 +6,13 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.parameter.parametersOf
 import ru.vsu.front.authorization.AuthScreen
 import ru.vsu.front.authorization.AuthViewModel
+import ru.vsu.front.profile.ProfileScreen
+import ru.vsu.front.profile.ProfileViewModel
 
 /**
  * Главный граф навигации приложения.
@@ -30,7 +34,24 @@ fun Navigation(
         composable<Route.Auth> {
             val viewModel = koinViewModel<AuthViewModel>()
             AuthScreen(
-                authViewModel = viewModel
+                authViewModel = viewModel,
+                onSuccessAuth = { userId ->
+                    navController.navigate(route = Route.Profile(userId))
+                }
+            )
+        }
+
+        composable<Route.Profile> { backStackEntry ->
+            val userId = backStackEntry.toRoute<Route.Profile>().userId
+
+            val viewModel = koinViewModel<ProfileViewModel>(
+                parameters = {
+                    parametersOf(userId)
+                }
+            )
+
+            ProfileScreen(
+                viewModel = viewModel,
             )
         }
     }
