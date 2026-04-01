3180cbcfd43efd6604c40c8be99417e49a47c456
stepann <stepan.snegovskoi@mail.ru>
2026-04-02 00:03:36 +0300

    update typography

--- frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/theme/Theme.kt
+++ frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/theme/Theme.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 /**
  * Корневой Composable-компонент темы приложения.
@@ -21,7 +23,13 @@ fun CodeTogetherTheme(
     }
 
     val typography = CodeTogetherTypography(
-        style = TextStyle(fontFamily = FontFamily.Monospace)
+        style = TextStyle(
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.5.sp
+        )
     )
 
     CompositionLocalProvider(
