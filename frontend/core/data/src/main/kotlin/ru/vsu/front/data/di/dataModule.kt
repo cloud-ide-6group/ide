8f46e5d6aeb3a1d6b917588514ae8b61eb44af17
stepann <stepan.snegovskoi@mail.ru>
2026-04-02 00:25:25 +0300

    add DefaultProfileRepository in dataModule

--- frontend/core/data/src/main/kotlin/ru/vsu/front/data/di/dataModule.kt
+++ frontend/core/data/src/main/kotlin/ru/vsu/front/data/di/dataModule.kt
@@ -3,16 +3,23 @@ package ru.vsu.front.data.di
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import ru.vsu.front.data.repository.DefaultAuthRepository
+import ru.vsu.front.data.repository.DefaultProfileRepository
 import ru.vsu.front.domain.repository.AuthRepository
+import ru.vsu.front.domain.repository.ProfileRepository
 
 /**
  * Модуль слоя data.
  * .
  * * Что внутри:
  * - [AuthRepository] - реализация репозитория через [DefaultAuthRepository].
+ * - [ProfileRepository] - реализация репозитория через [DefaultProfileRepository].
  */
 val dataModule = module {
     single {
         DefaultAuthRepository(get())
     }.bind<AuthRepository>()
+
+    single {
+        DefaultProfileRepository(get(), get())
+    }.bind<ProfileRepository>()
 }
