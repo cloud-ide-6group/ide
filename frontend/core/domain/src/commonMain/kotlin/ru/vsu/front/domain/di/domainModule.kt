ee79bb0e196a92fadb46c06ea79d3dd721a51af7
stepann <stepan.snegovskoi@mail.ru>
2026-04-02 00:35:08 +0300

    add GetProfileUseCase in domainModule

--- frontend/core/domain/src/commonMain/kotlin/ru/vsu/front/domain/di/domainModule.kt
+++ frontend/core/domain/src/commonMain/kotlin/ru/vsu/front/domain/di/domainModule.kt
@@ -1,6 +1,7 @@
 package ru.vsu.front.domain.di
 
 import org.koin.dsl.module
+import ru.vsu.front.domain.usecase.GetProfileUseCase
 import ru.vsu.front.domain.usecase.LoginUseCase
 import ru.vsu.front.domain.usecase.RefreshUseCase
 import ru.vsu.front.domain.usecase.SignUseCase
@@ -12,6 +13,7 @@ import ru.vsu.front.domain.usecase.SignUseCase
  * - [LoginUseCase] - Авторизация пользователя (вход в аккаунт).
  * - [SignUseCase] - Регистрация пользователя.
  * - [RefreshUseCase] - Обновление токенов.
+ * - [GetProfileUseCase] - Получение профиля пользователя.
  */
 val domainModule = module {
     single {
@@ -24,5 +26,9 @@ val domainModule = module {
 
     single {
         RefreshUseCase(get())
+    }
+
+    single {
+        GetProfileUseCase(get())
     }
 }
