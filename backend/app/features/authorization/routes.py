b6dc049bf9aca9bfe5c4c542567e4ad86b427a79
123abc1920 <123abc1920@mail.ru>
2026-04-11 18:59:43 +0300

    remove unneccessory from /sign

--- backend/app/features/authorization/routes.py
+++ backend/app/features/authorization/routes.py
@@ -103,15 +103,6 @@ def sign():
         schema:
           type: object
           properties:
-            name:
-              type: string
-              example: "username"
-            photo_path:
-              type: string
-              example: "users_imgs/default.png"
-            email:
-              type: string
-              example: "user@mail.ru"
             access_token:
               type: string
               example: "pbkdf2:sha256:260000$xyz..."
@@ -141,9 +132,6 @@ def sign():
     refresh_token = create_token(user.id, REFRESH_SECRET, timedelta(days=7), False)
 
     return {
-        "name": user.name,
-        "photo_path": user.photo_path,
-        "email": user.email,
         "access_token": access_token,
         "refresh_token": refresh_token,
     }, 201
