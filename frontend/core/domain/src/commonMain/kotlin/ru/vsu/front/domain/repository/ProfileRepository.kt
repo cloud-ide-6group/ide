e6e7e0c751c2a84d2cb650657dea91361a99f124
stepann <stepan.snegovskoi@mail.ru>
2026-04-25 02:13:10 +0300

    update ProfileRepository

--- frontend/core/domain/src/commonMain/kotlin/ru/vsu/front/domain/repository/ProfileRepository.kt
+++ frontend/core/domain/src/commonMain/kotlin/ru/vsu/front/domain/repository/ProfileRepository.kt
@@ -1,10 +1,10 @@
 package ru.vsu.front.domain.repository
 
 import ru.vsu.front.model.entity.Response
-import ru.vsu.front.model.entity.User
+import ru.vsu.front.model.entity.UserProfile
 
 /**
- * Интерфейс репозитория для аутентификации и регистрации.
+ * Интерфейс репозитория для работы с профилем.
  */
 interface ProfileRepository {
 
@@ -13,5 +13,29 @@ interface ProfileRepository {
 
      * @return [Response] с информацией о профиле пользователя, либо с ошибкой.
      */
-    suspend fun getProfile(userId: Int): Response<User>
+    suspend fun getProfile(): Response<UserProfile>
+
+    /**
+     * Выполняет получение профиля.
+
+     * @return [Response] с информацией об успехе запроса.
+     */
+    suspend fun updateProfileData(email: String, name: String): Response<*>
+
+    /**
+     * Выполняет получение профиля.
+
+     * @return [Response] с информацией об успехе запроса.
+     */
+    suspend fun updateProfilePassword(
+        newPassword: String,
+        oldPassword: String,
+    ): Response<*>
+
+    /**
+     * Выполняет получение профиля.
+
+     * @return [Response] с информацией об успехе запроса.
+     */
+    suspend fun updateProfilePhoto(photoBase64: String): Response<*>
 }
