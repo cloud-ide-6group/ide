e56c74c8d25ac079412fc51df155db8ee60872fb
stepann <stepan.snegovskoi@mail.ru>
2026-04-25 02:16:18 +0300

    update GetProfileUseCase

--- frontend/core/domain/src/commonMain/kotlin/ru/vsu/front/domain/usecase/GetProfileUseCase.kt
+++ frontend/core/domain/src/commonMain/kotlin/ru/vsu/front/domain/usecase/GetProfileUseCase.kt
@@ -2,7 +2,7 @@ package ru.vsu.front.domain.usecase
 
 import ru.vsu.front.domain.repository.ProfileRepository
 import ru.vsu.front.model.entity.Response
-import ru.vsu.front.model.entity.User
+import ru.vsu.front.model.entity.UserProfile
 
 /**
  * UseCase для получения профиля пользователя.
@@ -14,15 +14,12 @@ class GetProfileUseCase(
     private val repository: ProfileRepository,
 ) {
     /**
-     * Выполняет вход в систему.
-     *
-     * @param userId Айди пользователя.
+     * Выполняет получение профиля.
      *
      * @return [Response] с профилем пользователя при успехе, либо с ошибкой.
      */
     suspend operator fun invoke(
-        userId: Int,
-    ): Response<User> {
-        return repository.getProfile(userId = userId)
+    ): Response<UserProfile> {
+        return repository.getProfile()
     }
 }
