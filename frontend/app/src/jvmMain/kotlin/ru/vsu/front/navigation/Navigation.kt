d05d589fbfd4907bc6b5d930b6568b4181aed21e
stepann <stepan.snegovskoi@mail.ru>
2026-04-25 02:25:47 +0300

    update Navigation

--- frontend/app/src/jvmMain/kotlin/ru/vsu/front/navigation/Navigation.kt
+++ frontend/app/src/jvmMain/kotlin/ru/vsu/front/navigation/Navigation.kt
@@ -3,12 +3,15 @@ package ru.vsu.front.navigation
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
+import org.koin.compose.koinInject
 import org.koin.compose.viewmodel.koinViewModel
-import org.koin.core.parameter.parametersOf
+import ru.vsu.front.auth.AuthManager
+import ru.vsu.front.auth.AuthState
 import ru.vsu.front.authorization.AuthScreen
 import ru.vsu.front.authorization.AuthViewModel
 import ru.vsu.front.profile.ProfileScreen
@@ -18,14 +21,20 @@ import ru.vsu.front.profile.ProfileViewModel
  * Главный граф навигации приложения.
  *
  * @param navController Контроллер для управления стеком навигации.
+ * @param AuthManager Менеджер аутентификации, предоставляющий состояние текущей сессии пользователя.
  */
 @Composable
 fun Navigation(
     navController: NavHostController,
+    authManager: AuthManager = koinInject()
 ) {
+    val session by authManager.isAuthorized.collectAsStateWithLifecycle()
+
     NavHost(
         navController = navController,
-        startDestination = Route.Auth,
+        startDestination = if(session is AuthState.Authorized) {
+            Route.Profile((session as AuthState.Authorized).userId)
+        } else Route.Auth,
         enterTransition = { EnterTransition.None },
         exitTransition = { ExitTransition.None },
         popEnterTransition = { EnterTransition.None },
@@ -34,24 +43,14 @@ fun Navigation(
         composable<Route.Auth> {
             val viewModel = koinViewModel<AuthViewModel>()
             AuthScreen(
-                authViewModel = viewModel,
-                onSuccessAuth = { userId ->
-                    navController.navigate(route = Route.Profile(userId))
-                }
+                authViewModel = viewModel
             )
         }
 
-        composable<Route.Profile> { backStackEntry ->
-            val userId = backStackEntry.toRoute<Route.Profile>().userId
-
-            val viewModel = koinViewModel<ProfileViewModel>(
-                parameters = {
-                    parametersOf(userId)
-                }
-            )
-
+        composable<Route.Profile> {
+            val viewModel = koinViewModel<ProfileViewModel>()
             ProfileScreen(
-                viewModel = viewModel,
+                viewModel = viewModel
             )
         }
     }
