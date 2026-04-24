cb1b07b6815e2b42a60526928c75a807a94b2e5f
stepann <stepan.snegovskoi@mail.ru>
2026-04-25 02:17:59 +0300

    update appModule

--- frontend/app/src/jvmMain/kotlin/ru/vsu/front/di/appModule.kt
+++ frontend/app/src/jvmMain/kotlin/ru/vsu/front/di/appModule.kt
@@ -2,8 +2,16 @@ package ru.vsu.front.di
 
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import ru.vsu.front.common.dispatcher_provider.DefaultDispatcherProvider
+import ru.vsu.front.common.dispatcher_provider.DispatcherProvider
 import ru.vsu.front.config.BuildKonfig
 
+/**
+ * Модуль приложения.
+ *
+ * * Что внутри:
+ * - [BuildKonfig.BASE_URL] - базовый url для запросов.
+ */
 val appModule = module {
     single(named("baseUrl")) {
         BuildKonfig.BASE_URL
