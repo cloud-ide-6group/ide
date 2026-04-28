d1277a35237704d006bd9486bfeeedbaf08b23ef
stepann <stepan.snegovskoi@mail.ru>
2026-04-28 14:36:15 +0300

    update comments in ProfileRepository

--- frontend/core/domain/src/commonMain/kotlin/ru/vsu/front/domain/repository/ProfileRepository.kt
+++ frontend/core/domain/src/commonMain/kotlin/ru/vsu/front/domain/repository/ProfileRepository.kt
@@ -9,22 +9,25 @@ import ru.vsu.front.model.entity.UserProfile
 interface ProfileRepository {
 
     /**
-     * Выполняет получение профиля.
+     * Выполняет получение профиля пользователя.
 
      * @return [Response] с информацией о профиле пользователя, либо с ошибкой.
      */
     suspend fun getProfile(): Response<UserProfile>
 
     /**
-     * Выполняет получение профиля.
+     * Выполняет обновление почты и имени пользователя.
 
      * @return [Response] с информацией об успехе запроса.
      */
     suspend fun updateProfileData(email: String, name: String): Response<*>
 
     /**
-     * Выполняет получение профиля.
-
+     * Выполняет обновление пароля пользователя.
+     *
+     * @param newPassword Новый пароль.
+     * @param oldPassword Старый пароль.
+     *
      * @return [Response] с информацией об успехе запроса.
      */
     suspend fun updateProfilePassword(
@@ -34,7 +37,9 @@ interface ProfileRepository {
 
     /**
      * Выполняет получение профиля.
-
+     *
+     * @param photoBase64 Аватар в формате Base64.
+     *
      * @return [Response] с информацией об успехе запроса.
      */
     suspend fun updateProfilePhoto(photoBase64: String): Response<*>
