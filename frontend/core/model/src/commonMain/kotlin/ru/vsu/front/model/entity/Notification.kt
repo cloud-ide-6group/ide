f31a647cd35d7e3b7e25e0ddea6af25271319350
stepann <stepan.snegovskoi@mail.ru>
2026-05-05 19:16:50 +0300

    feat(model): add notification and notificationDTO entities

--- frontend/core/model/src/commonMain/kotlin/ru/vsu/front/model/entity/Notification.kt
+++ frontend/core/model/src/commonMain/kotlin/ru/vsu/front/model/entity/Notification.kt
@@ -1,8 +1,18 @@
 package ru.vsu.front.model.entity
 
+/**
+ * Уведомление (о приглашении в проект).
+ *
+ * @property notificationId Идентификатор уведомления.
+ * @property senderName Имя отправителя.
+ * @property sendTime Время отправки.
+ * @property projectId Идентификатор проекта, в который приглашён пользователь.
+ * @property projectName Название проекта, в который приглашён пользователь.
+ */
 data class Notification(
+    val notificationId: Int,
+    val senderName: String,
+    val sendTime: String,
     val projectId: Int,
     val projectName: String,
-    val ownerName: String,
-    val ownerPhoto: String,
 )
