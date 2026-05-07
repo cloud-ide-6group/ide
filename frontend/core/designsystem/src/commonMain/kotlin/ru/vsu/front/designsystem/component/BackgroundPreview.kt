f270f0e2ac4357e9bf7ee060399ced787fd593bd
stepann <stepan.snegovskoi@mail.ru>
2026-05-07 11:20:41 +0300

    style(designsystem): add padding to preview container

--- frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/component/BackgroundPreview.kt
+++ frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/component/BackgroundPreview.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.vsu.front.designsystem.theme.CodeTogetherTheme
 
 /**
@@ -23,7 +25,8 @@ fun BackgroundPreview(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(CodeTogetherTheme.colors.primaryBackground),
+                .background(CodeTogetherTheme.colors.primaryBackground)
+                .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
