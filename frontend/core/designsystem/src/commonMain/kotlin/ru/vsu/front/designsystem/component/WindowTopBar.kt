6c4462545c887464a4c04ac88a8709965872ed7c
stepann <stepan.snegovskoi@mail.ru>
2026-05-07 11:30:01 +0300

    feat(designsystem): make WindowTopBar clickable

--- frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/component/WindowTopBar.kt
+++ frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/component/WindowTopBar.kt
@@ -1,10 +1,13 @@
 package ru.vsu.front.designsystem.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,6 +41,12 @@ fun WindowScope.WindowTopBar(
             .fillMaxWidth()
             .height(NecessaryAppButtons.NECESSARY_BUTTON_SIZE_IN_DP.dp)
             .background(CodeTogetherTheme.colors.primaryBackground)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = {
+                }
+            )
     ) {
         Box(modifier = Modifier.fillMaxSize()) {
             Row(
