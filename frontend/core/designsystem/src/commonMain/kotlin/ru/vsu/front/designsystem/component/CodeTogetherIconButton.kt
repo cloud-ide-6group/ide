98c0ae116ff6e6f8a4f4087fdd568dbf61b9746b
stepann <stepan.snegovskoi@mail.ru>
2026-05-07 11:25:34 +0300

    feat(designsystem): add unhoverColor and preview to CodeTogetherIconButton

--- frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/component/CodeTogetherIconButton.kt
+++ frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/component/CodeTogetherIconButton.kt
@@ -3,6 +3,7 @@ package ru.vsu.front.designsystem.component
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
@@ -11,7 +12,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import front.core.designsystem.generated.resources.Res
+import front.core.designsystem.generated.resources.close_24dp
+import org.jetbrains.compose.resources.painterResource
+import ru.vsu.front.designsystem.theme.CodeTogetherTheme
+import kotlin.math.cos
 
 /**
  * Базовая кнопка приложения с иконкой.
@@ -20,6 +27,7 @@ import androidx.compose.ui.unit.dp
  * @param modifier Модификатор для настройки.
  * @param shape Форма кнопки.
  * @param hoverColor Цвет фона, который появляется при наведении курсора на кнопку.
+ * @param unhoverColor Цвет фона, когда курсор не на кнопке.
  * @param content Слот под контент.
  */
 @Composable
@@ -28,6 +36,7 @@ fun CodeTogetherIconButton(
     modifier: Modifier = Modifier,
     shape: Shape = RoundedCornerShape(8.dp),
     hoverColor: Color = Color.White.copy(alpha = 0.05f),
+    unhoverColor: Color = Color.Transparent,
     content: @Composable () -> Unit
 ) {
     val interactionSource = remember { MutableInteractionSource() }
@@ -39,9 +48,42 @@ fun CodeTogetherIconButton(
         shape = shape,
         interactionSource = interactionSource,
         colors = IconButtonDefaults.iconButtonColors(
-            containerColor = if (isHovered) hoverColor else Color.Transparent
+            containerColor = if (isHovered) hoverColor else unhoverColor
         )
     ) {
         content()
+    }
+}
+
+@Composable
+@Preview
+fun CodeTogetherIconButtonPreview() {
+    BackgroundPreview {
+        CodeTogetherIconButton(
+            onClick = {
+            },
+            modifier = Modifier,
+            shape = RoundedCornerShape(8.dp),
+        ) {
+            Icon(
+                painter = painterResource(Res.drawable.close_24dp),
+                contentDescription = null,
+                tint = CodeTogetherTheme.colors.primary
+            )
+        }
+
+        CodeTogetherIconButton(
+            onClick = {
+            },
+            modifier = Modifier,
+            shape = RoundedCornerShape(8.dp),
+            unhoverColor = Color.White.copy(alpha = 0.05f),
+        ) {
+            Icon(
+                painter = painterResource(Res.drawable.close_24dp),
+                contentDescription = null,
+                tint = CodeTogetherTheme.colors.primary
+            )
+        }
     }
 }
