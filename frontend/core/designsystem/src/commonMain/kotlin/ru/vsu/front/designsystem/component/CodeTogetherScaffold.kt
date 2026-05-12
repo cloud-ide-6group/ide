40d3b65b0badbefda0bdebdd6e53972f59d5f2c1
stepann <stepan.snegovskoi@mail.ru>
2026-05-12 12:57:11 +0300

    feat(app): add shadow to SnackbarHost

--- frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/component/CodeTogetherScaffold.kt
+++ frontend/core/designsystem/src/commonMain/kotlin/ru/vsu/front/designsystem/component/CodeTogetherScaffold.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush.Companion.linearGradient
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -63,6 +64,7 @@ fun CodeTogetherScaffold(
                         hostState = state,
                         modifier = Modifier
                             .padding(16.dp)
+                            .shadow(elevation = 8.dp, shape = CircleShape)
                             .clip(CircleShape),
                     ) {
                         CodeTogetherText(
