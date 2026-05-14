42c0307227ec294b34dd50fe849d3c7c09fd986e
stepann <stepan.snegovskoi@mail.ru>
2026-05-14 22:42:49 +0300

    feat(app): add settings button in all screens and add ProjectInfo in TopBarButtons

--- frontend/app/src/jvmMain/kotlin/ru/vsu/front/component/TopBarButtons.kt
+++ frontend/app/src/jvmMain/kotlin/ru/vsu/front/component/TopBarButtons.kt
@@ -7,6 +7,7 @@ import front.app.generated.resources.Res
 import front.app.generated.resources.back_24dp
 import front.app.generated.resources.logout_24dp
 import front.app.generated.resources.notifications_24dp
+import front.app.generated.resources.settings_24dp
 import ru.vsu.front.navigation.Route
 
 /**
@@ -27,10 +28,17 @@ fun TopBarButtons(
 ) {
     when {
         navDestination?.hasRoute<Route.Profile>() == true -> {
+            TopBarButton(onClick = {}, icon = Res.drawable.settings_24dp)
             TopBarButton(onClick = onLogoutClick, icon = Res.drawable.logout_24dp)
             TopBarButton(onClick = onNotificationsClick, icon = Res.drawable.notifications_24dp)
         }
         navDestination?.hasRoute<Route.Notifications>() == true -> {
+            TopBarButton(onClick = {}, icon = Res.drawable.settings_24dp)
+            TopBarButton(onClick = onLogoutClick, icon = Res.drawable.logout_24dp)
+            TopBarButton(onClick = onBackClick, icon = Res.drawable.back_24dp)
+        }
+        navDestination?.hasRoute<Route.ProjectInfo>() == true -> {
+            TopBarButton(onClick = {}, icon = Res.drawable.settings_24dp)
             TopBarButton(onClick = onLogoutClick, icon = Res.drawable.logout_24dp)
             TopBarButton(onClick = onBackClick, icon = Res.drawable.back_24dp)
         }
