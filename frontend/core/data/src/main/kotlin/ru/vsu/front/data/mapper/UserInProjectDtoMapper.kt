32d640ae5405a1f8196290c1be2a95da3b341531
stepann <stepan.snegovskoi@mail.ru>
2026-05-23 18:44:07 +0300

    feat(core/data): add mappers for UserDto and ProjectInfoDto

---
+++ frontend/core/data/src/main/kotlin/ru/vsu/front/data/mapper/UserInProjectDtoMapper.kt
@@ -0,0 +1,23 @@
+package ru.vsu.front.data.mapper
+
+import ru.vsu.front.data.entity.dto.UserDto
+import ru.vsu.front.model.entity.User
+
+/**
+ * Преобразует DTO-модель ([UserDto])
+ * в доменную сущность пользователя ([User]).
+ * * * Изолирует модели бизнес-логики от аннотаций `@Serializable` и `@SerialName`.
+ *
+ * * @return Модель [User], содержащая данные пользователя.
+ */
+fun UserDto.toEntity(): User {
+    return User(
+        userId = userId,
+        name = name
+    )
+}
+
+fun List<UserDto>.toEntities(): List<User> {
+    return map { it.toEntity() }
+}
+
