79f08fbbe2d4c072e2a7aec106a9f4c53ad6d216
stepann <stepan.snegovskoi@mail.ru>
2026-05-23 14:39:11 +0300

    feat(app): add projectModule to initKoin

--- frontend/app/src/jvmMain/kotlin/ru/vsu/front/di/initKoin.kt
+++ frontend/app/src/jvmMain/kotlin/ru/vsu/front/di/initKoin.kt
@@ -10,8 +10,8 @@ import ru.vsu.front.datastore.di.datastoreModule
 import ru.vsu.front.domain.di.domainModule
 import ru.vsu.front.network.di.networkModule
 import ru.vsu.front.notifications.di.notificationsModule
-import ru.vsu.front.notifications.di.projectModule
 import ru.vsu.front.profile.di.profileModule
+import ru.vsu.front.projects.di.projectModule
 
 /**
  * Инициализирует граф зависимостей Koin для всего приложения.
