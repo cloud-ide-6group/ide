b055635f449066fe6da73de9574d4ff90cd75d4a
stepann <stepan.snegovskoi@mail.ru>
2026-05-29 00:58:33 +0300

    fix(feature/projectinfo): fix height of MemberItem

--- frontend/feature/projectInfo/src/commonMain/kotlin/ru/vsu/front/projectinfo/component/MemberItem.kt
+++ frontend/feature/projectInfo/src/commonMain/kotlin/ru/vsu/front/projectinfo/component/MemberItem.kt
@@ -3,6 +3,7 @@ package ru.vsu.front.projectinfo.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -54,6 +55,8 @@ fun MemberItem(
                     tint = CodeTogetherTheme.colors.primary,
                 )
             }
+        } else {
+            Spacer(modifier = Modifier.height(24.dp))
         }
     }
 }
